060c5a7e56109c18a319227a359c769726b98632
Smael Nicolas <nico.smael@gmail.com>
2021-11-14 13:48:58 -0300

    fix card widths

--- src/components/container/ItemListContainer/Card/Amount/Amount.jsx
+++ src/components/container/ItemListContainer/Card/Amount/Amount.jsx
@@ -4,7 +4,7 @@ function Amount() {
 			<i class="fas fa-arrow-down productCardAmountArrow"></i>
 			<input className="productCardAmountInput" type="text" />
 			<i class="fas fa-arrow-up productCardAmountArrow"></i>
-			<i class="fas fa-luggage-cart productCardAmountBuy"></i>
+			<i class="fas fa-cart-plus productCardAmountBuy"></i>
 		</div>
 	);
 }
--- src/functions/getFetch.js
+++ src/functions/getFetch.js
@@ -3,5 +3,5 @@ import { productos } from "../data/productos";
 export const getFetch = new Promise((accept, deny) => {
 	setTimeout(() => {
 		accept(productos);
-	}, 4000);
+	}, 3000);
 });
