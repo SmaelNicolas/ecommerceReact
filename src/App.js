a86f34194bdd88cb75c8b3ae5313def2f6d59223
Smael Nicolas <nico.smael@gmail.com>
2021-11-20 18:05:13 -0300

    links

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
 						path="/productos/:idCategoria"
 						element={<ItemListContainer />}
 					/>
+					<Route exact path="/descripcion/:idProducto" />
 				</Routes>
 			</div>
 		</BrowserRouter>
