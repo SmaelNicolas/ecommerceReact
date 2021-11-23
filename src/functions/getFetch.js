f8390f2b39d180eb32c37058d6528e92dd830009
Smael Nicolas <nico.smael@gmail.com>
2021-11-23 17:11:50 -0300

    fix cursor, add loading in sections

--- src/functions/getFetch.js
+++ src/functions/getFetch.js
@@ -1,8 +1,5 @@
 import { productos } from "../data/productos";
 
 export const getFetch = new Promise((accept, deny) => {
-	// accept(productos);
-	// setTimeout(() => {
 	accept(productos);
-	// }, 3000);
 });
