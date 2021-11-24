d87e8906c947c1a86fb24942f50512062af8bd7d
Smael Nicolas <nico.smael@gmail.com>
2021-11-23 21:14:42 -0300

    act

--- src/components/ItemDescriptionContainer/ItemDescriptionContainer.jsx
+++ src/components/ItemDescriptionContainer/ItemDescriptionContainer.jsx
@@ -33,9 +33,7 @@ function ItemDescriptionContainer() {
 	}, [idProducto]);
 
 	function listToDisplay() {
-		return loader ? (
-			<LoadingScreen />
-		) : (
+		return (
 			<section className="ItemDescriptionContainer">
 				<div
 					key={`description${productoDescripcion.id}`}
