7c3ac866e4375ac50de6e6795689a0f07bd89816
Smael Nicolas <nico.smael@gmail.com>
2021-12-13 14:09:23 -0300

    msj valid screen

---
+++ src/components/CartContainer/Purchase/ValidPurchaseScreen/ValidPurchaseScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./ValidPurchaseScreen.css";
+
+function ValidPurchaseScreen() {
+	return (
+		<section className="ValidPurchaseScreen">
+			<p>COMPRA EXITOSA</p>
+			<button>HOME</button>
+		</section>
+	);
+}
+
+export default ValidPurchaseScreen;
